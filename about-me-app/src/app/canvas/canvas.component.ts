f8bca2efaf2074c7d5232ceba5ec2a1b406f8260
Alexander Morton <mortonprod@gmail.com>
2018-04-29 21:28:54 +0100

    Add example jpg over sphere segment

--- about-me-app/src/app/canvas/canvas.component.ts
+++ about-me-app/src/app/canvas/canvas.component.ts
@@ -19,18 +19,24 @@ export class CanvasComponent implements AfterContentInit {
     this.angleX = 0;
     this.angleY = 0;
   }
-  ngAfterContentInit() {
+  async ngAfterContentInit() {
     this.el = document.getElementById('canvas');
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(75, this.el.clientWidth / this.el.clientHeight, 0.1, 1000);
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(this.el.clientWidth, this.el.clientHeight);
     this.el.appendChild(this.renderer.domElement);
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: '#66023C' });
+    // const geometry = new THREE.SphereGeometry(2, 32, 32);
+    // const geometry = new THREE.SphereGeometry(3, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
+    // const geometry = new THREE.SphereGeometry(3, 10, 10, 0, Math.PI * 2, 0, Math.PI * 2);
+    const geometry = new THREE.SphereGeometry( 5, 24, 16, 0 * Math.PI/2, Math.PI/2 );
+    // const material = new THREE.MeshBasicMaterial({ color: '#66023C' });
+    const texture = await new THREE.TextureLoader().load( 'assets/Rectangle.jpg' );
+    // const material = new THREE.MeshNormalMaterial();
+    const material = new THREE.MeshBasicMaterial( { map: texture } );
     this.cube = new THREE.Mesh(geometry, material);
     this.scene.add(this.cube);
-    this.camera.position.z = 5;
+    this.camera.position.z = 10;
     this.animate();
   }
   onResize(event) {
