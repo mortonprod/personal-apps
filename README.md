df7825ae7cea9880c9ba83ad7b3515b6e1491def
Alexander Morton <mortonprod@gmail.com>
2018-08-05 12:11:43 +0100

    Update upload script

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 
 Pushing assets to s3 bucket.
 ```
-export AWS_PROFILE=personal 
-aws s3 cp ./*.html ./css/* ./js/* ./images/* ./fonts/**/*  s3://personal-apps --recursive
+bash upload.sh
 ```
 
-Must make sure it follows the same  folder structure as html served.
+Must make sure it follows the same folder structure as html served. This is important with SPA so run with
+
+```
+ng build --prod -d /personal-pwa
+```
--- upload.sh
+++ upload.sh
@@ -6,4 +6,7 @@ aws s3 sync ./images s3://personal-apps/images
 aws s3 sync ./fonts s3://personal-apps/fonts
 # Need to exlude everything. Then add all html. The exclude all files in directory below. Stupid aws cli.
 aws s3 sync . s3://personal-apps/ --exclude "*" --include "*.html" --exclude "*/*"
-# aws s3 cp --include ./*.html ./css/* ./js/* ./images/* ./fonts/**/*  s3://personal-apps --recursive
+
+# Now push pwa dist folder
+aws s3 sync ./personal-pwa/dist s3://personal-apps/personal-pwa
+
